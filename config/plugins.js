51db972df50c662513af057b57a682a836fa5924
Deme <demeyodaniel0@gmail.com>
2024-06-04 10:52:29 +0530

    disabled cache

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = {
+    "rest-cache": {
+    config: {
+        provider: {
+            name: "memory",
+            options: {
+                max: 0,
+                maxAge: 0,
+            },
+        },
+        strategy: {
+        contentTypes: [
+                // list of Content-Types UID to cache
+                // "api::category.category",
+                // "api::article.article",
+                // "api::global.global",
+                // "api::homepage.homepage",
+            ],
+        },
+    },
+    },
+};
